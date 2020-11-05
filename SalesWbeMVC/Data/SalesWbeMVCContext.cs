482fb84cf847abb49814fa17c1d4d44071d0d3bf
GabrielJTavares <gabriel-tavares1996@outlook.com>
2020-11-05 12:45:11 -0300

    crud scaffold

---
+++ SalesWbeMVC/Data/SalesWbeMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWbeMVC.Models;
+
+namespace SalesWbeMVC.Data
+{
+    public class SalesWbeMVCContext : DbContext
+    {
+        public SalesWbeMVCContext (DbContextOptions<SalesWbeMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWbeMVC.Models.Departament> Departament { get; set; }
+    }
+}
