a84c8138c2ca0e529d93ae3755f2cdf0e5ed3e48
GabrielJTavares <gabriel-tavares1996@outlook.com>
2020-11-16 15:33:23 -0300

    Formulario de cadastro de funcionário

--- SalesWbeMVC/Services/SellerService.cs
+++ SalesWbeMVC/Services/SellerService.cs
@@ -20,6 +20,11 @@ namespace SalesWbeMVC.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
         
     }
 }
