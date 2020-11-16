03353baf9bddfba61bf4931838dc09122ded1f5d
GabrielJTavares <gabriel-tavares1996@outlook.com>
2020-11-16 14:50:59 -0300

    Tabela de vendedores

---
+++ SalesWbeMVC/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWbeMVC.Data;
+using SalesWbeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWbeMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWbeMVCContext _context;
+
+        public SellerService(SalesWbeMVCContext context)
+        {
+            _context = context;
+        } 
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+        
+    }
+}
--- SalesWbeMVC/Startup.cs
+++ SalesWbeMVC/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWbeMVC.Data;
+using SalesWbeMVC.Services;
 
 namespace SalesWbeMVC
 {
@@ -41,6 +42,7 @@ namespace SalesWbeMVC
                         builder.MigrationsAssembly("SalesWbeMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
