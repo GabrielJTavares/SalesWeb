286316086024d4b14b73b538b793362974c98227
GabrielJTavares <gabriel-tavares1996@outlook.com>
2021-01-12 16:25:39 -0300

    View Simple Search

---
+++ SalesWbeMVC/Services/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using SalesWbeMVC.Data;
+using System;
+using SalesWbeMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWbeMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWbeMVCContext _context;
+
+        public SalesRecordService(SalesWbeMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x=> x.Seller)
+                .Include(x=>x.Seller.Departament)
+                .OrderByDescending(x=>x.Date)
+                .ToListAsync();
+        }
+    }
+}
